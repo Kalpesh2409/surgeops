075f4b533621a63a17b645c6d5cd1b4be4c11e7a
Kalpesh2409 <kalpeshwahurwagh@gmail.com>
2026-09-07 11:42:14 +0530

    Add Node types reference to remaining scripts to fix process type errors

--- apps/api/scripts/resetDemoData.ts
+++ apps/api/scripts/resetDemoData.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 /**
  * resetDemoData.ts
  *
--- apps/api/scripts/seedHistory.ts
+++ apps/api/scripts/seedHistory.ts
@@ -1,3 +1,5 @@
+/// <reference types="node" />
+
 import "dotenv/config";
 import { PrismaClient, OrderStatus } from "@prisma/client";
 
